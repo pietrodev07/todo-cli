4635e2e194545bb090db8dffbf7daa75d33bcbf3
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-12 13:38:08 +0200

    feat(todo): create remove-list method

--- todo/todo.go
+++ todo/todo.go
@@ -1,5 +1,9 @@
 package todo
 
+import (
+	"fmt"
+)
+
 // Task represents a single to-do task.
 type Task struct {
 	Title     string
@@ -14,4 +18,14 @@ type TaskList struct {
 // Add adds a new task to the list.
 func (t *TaskList) Add(title string) {
 	t.Items = append(t.Items, Task{Title: title, Completed: false})
+}
+
+// Remove removes a task by its index from the list.
+func (t *TaskList) Remove(index int) (string, error) {
+	if index < 0 || index >= len(t.Items) {
+		return "", fmt.Errorf("index out of range")
+	}
+	task := t.Items[index].Title
+	t.Items = append(t.Items[:index], t.Items[index+1:]...)
+	return task, nil
 }
